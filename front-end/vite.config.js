2609f7f50154aca4236d82073596a6be68d10b5d
LuccaPeregrino <lucca.peregrino@hotmail.com>
2025-12-29 03:35:19 -0300

    alteracao arquivos

---
+++ front-end/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: "esnext",
+    rollupOptions: {
+      // força o Rollup JS (evita binário nativo)
+      plugins: []
+    }
+  }
+});
